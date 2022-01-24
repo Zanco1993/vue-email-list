7f0f4962c5d64f8356f9157437113455868576d4
Zanco1993 <zanconatofabio@gmail.com>
2022-01-24 14:51:24 +0100

    es completato

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Descrizione:
+// Attraverso l’apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+new Vue ({
+    el: "#app",
+    data: {
+        listMail: [],
+        
+    },
+
+    methods: {
+      emailGenerator: function() {
+        for(i = 0;i < 10;i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((email) => {
+                this.listMail.push(email.data.response)
+            })
+
+        }
+      },
+
+      
+
+    },
+    
+    
+})
